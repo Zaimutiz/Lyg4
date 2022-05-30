d6572420d5a9ac7bb3599ecfd4a1b5b86e19605a
Zaimutiz <zaimutis@gmail.com>
2022-05-30 03:40:07 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -2,7 +2,8 @@
 #SBATCH -p short
 #SBATCH -n64
 #SBATCH -C beta
+#SBATCH --time=2
 #SBATCH -o MPI_DYNAMIC_RESULTS
 #SBATCH -e MPI_DYNAMIC_ERRORS
-mpic++ -pthread -o PrimeSieve PrimeSieve.cpp
-mpirun PrimeSieve 
+mpic++ -pthread -o MPI_DYNAMIC PrimeSieve.cpp
+mpirun MPI_DYNAMIC
